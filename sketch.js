d2e889ffa2be83299d7b622296a5ed7fcc17c84a
aneetayadv <70881389+aneetayadv@users.noreply.github.com>
2020-10-16 12:27:40 +0530

    Update sketch.js
    Stop car just when its touching the wall

--- sketch.js
+++ sketch.js
@@ -21,6 +21,8 @@ function draw() {
   if(wall.x-car.x < car.width/2 + wall.width/2)
   {
     car.velocityX = 0;
+    car.x = wall.x - ((car.width + wall.width)/2);
+    
     var deformation = 0.5 * weight * speed * speed/ 22500;
     console.log(deformation);
     if(deformation > 180)
